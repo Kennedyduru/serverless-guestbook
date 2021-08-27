991d25a14f34107755a287582c580c0f6d86c681
Kennedyduru <55640696+Kennedyduru@users.noreply.github.com>
2021-08-27 17:42:16 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,8 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = '
+https://30f0580e.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
